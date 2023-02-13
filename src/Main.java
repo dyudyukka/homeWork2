240ffe98cbdd6f6854c9bd3d4ebab972c99a6954
User <linax1979@mail.ru>
2023-02-13 22:54:51 +1000

    исправлен - на +

--- src/Main.java
+++ src/Main.java
@@ -57,8 +57,8 @@ public class Main {
         // Пишем код для задачи 4
         var friend = 19;
         System.out.println ("friend = "+friend);
-        friend = friend * 2;
-        System.out.println ("friend * 2 = "+friend);
+        friend = friend + 2;
+        System.out.println ("friend + 2 = "+friend);
         friend = friend /7;
         System.out.println ("friend /7 = "+friend);
     }
